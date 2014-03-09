a5d5c04ca51b6d993f5a500a012f3b2ec56aaca3
Puru Dahal <pdahal@me.com>
2014-03-08 23:52:39 -0600

    change method name

--- lib/gr_avatar.rb
+++ lib/gr_avatar.rb
@@ -2,7 +2,7 @@ require "gr_avatar/version"
 require 'digest/md5'
 
 module GrAvatar
-	def gravatar_url(user_email)
+	def self.gr_avatar(user_email)
 	  	gravatar_id = Digest::MD5.hexdigest(user_email.downcase)
 		"http://gravatar.com/avatar/#{gravatar_id}.png"
 	end
